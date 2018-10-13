d9f396a8728653f20baa9b697bace6e0bc8dff95
Samantha <scdikolli@gmail.com>
2018-10-13 11:35:53 -0400

    new quiz slick

--- js/scripts.js
+++ js/scripts.js
@@ -11,53 +11,35 @@ $('.single-item').slick();
 
 });
 
-$('#quiz').quiz({
-  //resultsScreen: '#results-screen',
-  //counter: false,
-  //homeButton: '#custom-home',
-  counterFormat: 'Question %current of %total',
-  questions: [
-    {
-      'q': 'Is jQuery required for this plugin?',
-      'options': [
-        'Yes',
-        'No'
-      ],
-      'correctIndex': 0,
-      'correctResponse': 'Good job, that was obvious.',
-      'incorrectResponse': 'Well, if you don\'t include it, your quiz won\'t work'
+var quizJSON = {
+    "info": {
+        "name":    "The Quiz Header",
+        "main":    "The Quiz Description Text",
+        "results": "The Quiz Results Copy",
+        "level1":  "The highest ranking",
+        "level2":  "The almost highest ranking",
+        "level3":  "The middle ranking",
+        "level4":  "The almost lowest ranking",
+        "level5":  "The lowest ranking"
     },
-    {
-      'q': 'How do you use it?',
-      'options': [
-        'Include jQuery, that\'s it!',
-        'Include jQuery and the plugin javascript.',
-        'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Sounds more complicated than it really is.',
-      'incorrectResponse': 'Come on, it\'s not that easy!'
-    },
-    {
-      'q': 'The plugin can be configured to require a perfect score.',
-      'options': [
-        'True',
-        'False'
-      ],
-      'correctIndex': 0,
-      'correctResponse': 'You\'re a genius! You just set allowIncorrect to true.',
-      'incorrectResponse': 'Why you have no faith!? Just set allowIncorrect to true.'
-    },
-    {
-      'q': 'How do you specify the questions and answers?',
-      'options': [
-        'MySQL database',
-        'In the HTML',
-        'In the javascript configuration'
-      ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Refer to the documentation for the structure.',
-      'incorrectResponse': 'Wrong! Do it in the javascript configuration. You might need to read the documentation.'
-    }
-  ]
+    "questions": [
+        {
+            "q": "The Question?",
+            "a": [
+                {"option": "an incorrect answer",       "correct": false},
+                {"option": "a correct answer",          "correct": true},
+                {"option": "another correct answer",    "correct": true}
+            ],
+            "correct": "The Correct Response Message",
+            "incorrect": "The Incorrect Response Message",
+            "select_any": false, // optional, see "Question Options" above
+            "force_checkbox": false // optional, see "Question Options" above
+        }
+    ]
+}
+
+$(function () {
+    $('#slickQuiz').slickQuiz({
+        // options
+    });
 });
